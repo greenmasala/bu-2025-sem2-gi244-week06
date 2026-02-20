ab4fed07a0351cd4cdf0e6aed5ddc1d8b32fdfce
greenmasala <sinchai2549@gmail.com>
2026-02-20 10:55:05 +0700

    Exam06 partially done

--- Assets/Scripts/Exam/CameraControlExam06.cs
+++ Assets/Scripts/Exam/CameraControlExam06.cs
@@ -5,14 +5,43 @@ public class CameraControlExam06 : MonoBehaviour
     public GameObject player1;
     public GameObject player2;
     public float offset;
+    private float cameraBound;
     public Camera targetCamera;
 
     // Update is called once per frame
+    private void Start()
+    {
+ 
+    }
     void LateUpdate()
     {
         Vector3 player1Pos = player1.transform.position;
         Vector3 player2Pos = player2.transform.position;
 
+        targetCamera.transform.position = new Vector3 (player1Pos.x, targetCamera.transform.position.y, player2Pos.z);
+        float cameraOffsetZ = player2Pos.z - player1Pos.z;
+        float cameraOffsetX = player1Pos.x - player2Pos.x;
+        float cameraSize = cameraOffsetX + cameraOffsetZ;
+
+        if (player2Pos.z <= 10)
+        {
+            cameraSize = 10;
+        }
+        else if (player2Pos.z <= 0 || player2Pos.z > 10)
+        {
+            targetCamera.orthographicSize = cameraSize + offset;
+        }
+        if (player1Pos.x < 10)
+        {
+            cameraSize = 10;
+        }
+        else if (player1Pos.x < -10 || player2Pos.x > 10)
+        {
+            targetCamera.orthographicSize = cameraSize + offset;
+        }
+        
+
+        //targetCamera.orthographicSize = player2Pos.z + offset;
         // Student code ...
     }
 }
